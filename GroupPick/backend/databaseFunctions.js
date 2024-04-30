5d443e55a12f89706aa3c8d972d37499df5537b0
bwiss11 <bwiss11@gmail.com>
2024-04-30 18:23:00 -0400

    backend connected to mongodb and retrieving groups by their assigned _id in mongodb.

--- GroupPick/backend/databaseFunctions.js
+++ GroupPick/backend/databaseFunctions.js
@@ -1,38 +1,25 @@
 // mongodb+srv://bwiss11:Morioles!1@cluster0.klspkku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId, insertOne } = require("mongodb");
 // Replace the uri string with your connection string.
 const uri =
   "mongodb+srv://bwiss11:Morioles!1@cluster0.klspkku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
-async function run() {
-  try {
-    const database = client.db("GroupPick");
-    const users = database.collection("users");
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { email: "bwiss11@gmail.com" };
-    const user = await users.findOne(query);
-    console.log(user);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
 
 async function logPick() {
   try {
+    await client.connect();
     const database = client.db("GroupPick");
-    const users = database.collection("users");
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { email: "bwiss11@gmail.com" };
-    const user = await users.findOne(query);
-    // const picks = await user.find({ picks: "20240429" });
-    console.log(
-      user.picks["20240429"]
-      // .project({ picks: "20240429" })
-    );
+    const groups = database.collection("groups");
+
+    id = new ObjectId("663160c17bebb241a07623dc");
+    const ans2 = await groups.findOne({
+      _id: id,
+    });
+
+    console.log(ans2);
   } finally {
     await client.close();
   }
 }
-logPick().catch(console.dir);
+// run().catch(console.dir);
+logPick();
