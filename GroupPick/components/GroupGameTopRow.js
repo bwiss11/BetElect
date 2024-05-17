ca3b70961d5e589d9a98c179517d07bf61d26406
bwiss11 <bwiss11@gmail.com>
2024-05-17 16:47:11 -0400

    Changed checkbox icon from svg to be expo compatible.

--- GroupPick/components/GroupGameTopRow.js
+++ GroupPick/components/GroupGameTopRow.js
@@ -14,6 +14,7 @@ import {
 import { GetFormattedDate } from "../backend/functions";
 import { Avatar, Title, Caption, TouchableRipple } from "react-native-paper";
 import GroupGameAvatar from "./GroupGameAvatar";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const userToPicksId = {
   L2tcqkRGYEEHb20DVbv5: "JU9K63mDllpPQbDt1Gx9",
@@ -80,17 +81,7 @@ const GroupGameTopRow = (props) => {
       </View>
       <View style={styles.check}>
         {picksIn == 3 ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="20"
-            height="20"
-            viewBox="0 0 50 50"
-            fill="rgb(80, 200, 120)"
-          >
-            <path d="M 11 4 C 7.101563 4 4 7.101563 4 11 L 4 39 C 4 42.898438 7.101563 46 11 46 L 39 46 C 42.898438 46 46 42.898438 46 39 L 46 15 L 44 17.3125 L 44 39 C 44 41.800781 41.800781 44 39 44 L 11 44 C 8.199219 44 6 41.800781 6 39 L 6 11 C 6 8.199219 8.199219 6 11 6 L 37.40625 6 L 39 4 Z M 43.25 7.75 L 23.90625 30.5625 L 15.78125 22.96875 L 14.40625 24.4375 L 23.3125 32.71875 L 24.09375 33.4375 L 24.75 32.65625 L 44.75 9.03125 Z"></path>
-          </svg>
+          <Ionicons name="checkbox-outline" size={20} color="rgb(80,200,120)" />
         ) : (
           <View></View>
         )}
@@ -139,5 +130,6 @@ const styles = StyleSheet.create({
   check: {
     color: "white",
     justifyContent: "center",
+    paddingTop: 10,
   },
 });
